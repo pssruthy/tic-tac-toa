f88e4fff3979549a38d1a47cde6e316230f89d95
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-08-27 21:54:14 +0530

    Changed the style

--- src/Status.js
+++ src/Status.js
@@ -4,7 +4,7 @@ const Status = ({ player, winner, isDrawn }) => {
   let className = '';
   let message = `${player}'s turn`;
   if (isDrawn) {
-    className = 'draw gameOver';
+    className = 'drawn gameOver';
     message = 'Drawn';
   }
   if (winner !== null) {
